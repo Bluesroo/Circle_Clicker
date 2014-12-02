5392ea52bcfef678d635e8db96c9ac83b43cdc1a
bluesroo <jpariseau101@gmail.com>
2014-12-01 22:38:34 -0800

    A little refactoring.

--- README.md
+++ README.md
@@ -3,5 +3,13 @@
 The point of this game is to click 10 randomly generated squares which pop up on the screen. It is very basic and I used
 it to teach myself a little about C++, event based programming, and graphics programming (with the help of [SFML](http://sfml-dev.org/index.php)).
 
+##Files
+###[game.cpp](https://github.com/Bluesroo/Circle_Clicker/blob/master/game.cpp) and [game.h](https://github.com/Bluesroo/Circle_Clicker/blob/master/game.h)
+game.cpp is the main entry point for the program and game.h contains the important declarations and includes.
+###[circleClicker.cpp](https://github.com/Bluesroo/Circle_Clicker/blob/master/circleClicker.cpp) and [endOfGame.cpp](https://github.com/Bluesroo/Circle_Clicker/blob/master/endOfGame.cpp)
+These files are the game and endgame screen.
+###[wrappers.h](https://github.com/Bluesroo/Circle_Clicker/blob/master/wrappers.h)
+wrappers.h contains a couple classes that were extended in order to help with the functionality.
+
 ####Note:
-    This is a very old project and will only compile on Linux (I believe)
+    This is a very old project and will only compile on Linux (I believe).
--- game.cpp
+++ game.cpp
@@ -1,61 +1,25 @@
 #include "main.h"
 #include "wrappers.h"
 
-int game(sf::RenderWindow *window)
-{
-    //Sets mouse position at start
-    sf::Mouse::setPosition(sf::Vector2i(WIDTH/2, HEIGHT/2), *window);
-    //Sets up the timer and round counter
-    int roundCount = 0;
-    //Generates the shape
-    ShapeWrapper circle;
-    circle.setSize(sf::Vector2f(50, 50));
-    circle.setPosition(rand()%(WIDTH - 71), rand()%(HEIGHT - 71));
+int main() {
+    //Creates the window
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "My window");
+    //Seeds random
+    std::srand(time(NULL));
+    //Initialize important variables
+    int done = 0;
+    clock_t timerStart;
 
-    while (1) {
-        //Checks all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window->pollEvent(event)) {
-            switch (event.type) {
-                //Handles window close
-                case sf::Event::Closed:
-                    window->close();
-                    return 1;
-                    break;
+    //Run the program as long as the window is open
+    while (window.isOpen() && done != 1) {
+        //Gets time at the start of the game
+        timerStart = clock();
+        //Game loop
+        done = game(&window);
 
-                //Handles mouse events
-                case sf::Event::MouseButtonPressed:
-                    if (event.mouseButton.button == sf::Mouse::Left) {
-                        if (circle.checkShape(window)) {
-                            circle.setPosition(rand()%(WIDTH - 71), rand()%(HEIGHT - 71));
-                            roundCount++;
-                            if (roundCount > 9)
-                                return 0;
-                        }
-                    }
-                    if (event.mouseButton.button == sf::Mouse::Right) {
-                        return 2;
-                    }
-                    break;
-
-                //Handles keyboard events
-                case sf::Event::KeyPressed:
-                    if (event.key.code == sf::Keyboard::Escape) {
-                        window->close();
-                        return 1;
-                    }
-                    break;
-
-                //If there are no events
-                default:
-                    break;
-            }
-        }
-
-        //Puts stuff on the screen
-        window->clear(sf::Color::Blue);
-        window->draw(circle);
-        window->display();
+        if (done == 0)
+            //Finds out whether the player wants another round
+            done = endOfGame(&window, timerStart);
     }
 
     return 0;
